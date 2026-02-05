839a01664816d9646cdcd1a5e177d371cd4c728a
luisotavioreis102-a11y <luisotavioreis102@gmail.com>
2026-02-04 21:12:11 -0300

    Create script.js

---
+++ ex001/script.js
@@ -0,0 +1,13 @@
+var a = 5 + 5
+var b = 5 * 5
+var c = 5 / 5
+var d = 5 - 5
+var e = a * b
+var f = c + a
+
+console.log(` O resultado de todas essas contas é: ${a}, ${b}, ${c}, ${d}, ${e}, ${f}`)
+
+var media = 7
+var status = media >= 7 ? "Aprovado" : "Reprovado";
+console.log(`O aluno está: ${status}`);
+
